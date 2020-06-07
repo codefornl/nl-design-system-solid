63c73aa722fa79d1075b1f39436a685770cecfd3
milovanderlinden <milo@dogodigi.net>
2020-06-07 12:32:03 +0200

    Notification component that can be closed on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Header, HeaderProperties } from './components/header/header';
+import { Header } from './components/header/header';
 import { SkipLink } from './components/skiplink/skliplink';
 import { Notification } from './components/notification/notification';
+import { Show, createState } from 'solid-js';
 
 function App() {
+
 
   return (
     <>
@@ -22,10 +24,16 @@ function App() {
           <article>
             <div class="row">
               <div class="col-sm-12">
-                <Notification 
-                  type="warning" 
-                  title="NL Design System in pilotfase"
-                  description={`Het NL Design System bevindt zich momenteel nog in de pilotfase. Hierdoor is het nu nog niet volwassen genoeg om volledig gebruikt te worden. Houdt hier dus rekening mee voor je aan de slag gaat. We zouden het op prijs stellen als je wilt bijdragen om het NLDS tot een succes te maken. Dit kan in onze gitlab of met een mailtje aan: NLDesignSystem@gebruikercentraal.nl`}/>
+                  <Notification 
+                    type="warning" 
+                    title="NL Design System in pilotfase"
+                    description={`Het NL Design System bevindt 
+                    zich momenteel nog in de pilotfase. 
+                    Hierdoor is het nu nog niet volwassen genoeg om volledig gebruikt 
+                    te worden. Houdt hier dus rekening mee voor je aan de slag gaat. We 
+                    zouden het op prijs stellen als je wilt bijdragen om het NLDS tot een 
+                    succes te maken. Dit kan in onze gitlab of met een 
+                    mailtje aan: NLDesignSystem@gebruikercentraal.nl`}/>
               </div>
             </div>
           </article>
--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -1,4 +1,5 @@
 import './notification.scss'
+import { createState, Show } from 'solid-js';
 
 interface Properties {
     type: 'success' | 'info' | 'warning' | 'error' //'TER INFORMATIE' | 'LET OP' | 'GELUKT' | 'STORING';
@@ -12,6 +13,11 @@ interface Properties {
  * Listens for click events on the close button for notification to close the notification using javascript
  */
 export const Notification = (properties: Properties) => {
+    const [state, setState] = createState({ notification: true });
+
+    const handleDismiss = () => {
+        setState({ notification: false })
+    }
     let typeTitle: string
     switch (properties.type) {
         case 'success': {
@@ -35,26 +41,22 @@ export const Notification = (properties: Properties) => {
      * Closes the notification
      */
     const close = () => {
-        console.log("click");
+        handleDismiss()
     }
 
     return (
-        <div class={`notification notification--${properties.type}`} id={properties.id}>
-            <div class="notification__content">
-                <div class="notification__type">{typeTitle}</div>
+        <Show when={state.notification}>
+            <div class={`notification notification--${properties.type}`} id={properties.id}>
+                <div class="notification__content">
+                    <div class="notification__type">{typeTitle}</div>
                     <h2 class="notification__title">{properties.title}</h2>
                     <span>{properties.description}</span>
-            </div>
-            <button class="notification__close-button" onclick={close}>
-                <i class="icon icon-cross"></i>
+                </div>
+                <button class="notification__close-button" onclick={close}>
+                    <i class="icon icon-cross"></i>
                 Sluit
             </button>
-        </div>
+            </div>
+        </Show>
     );
 };
-
-
-
-
-
-
